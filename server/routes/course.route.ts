511aca560deeff5f5810c5e85bfc21e40907fd9d
Diego Bonilla <drbv27@gmail.com>
2023-11-14 17:37:12 -0500

    feat: delete course, delete user

--- server/routes/course.route.ts
+++ server/routes/course.route.ts
@@ -4,6 +4,7 @@ import {
     addQuestion,
     addReplyToReview,
     addReview,
+    deleteCourse,
     editCourse,
     getAllCourses,
     getCourseByUser,
@@ -61,6 +62,13 @@ courseRouter.put(
     isAuthenticated,
     authorizeRoles("admin"),
     getAllCourses
+  );
+
+  courseRouter.delete(
+    "/delete-course/:id",
+    isAuthenticated,
+    authorizeRoles("admin"),
+    deleteCourse
   );
 
 export default courseRouter;
--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { activateUser, getAllUsers, getUserInfo, loginUser, logoutUser, registrationUser, socialAuth, updateAccessToken, updatePassword, updateProfilePicture, updateUserInfo, updateUserRole } from "../controllers/user.controller";
+import { activateUser, deleteUser, getAllUsers, getUserInfo, loginUser, logoutUser, registrationUser, socialAuth, updateAccessToken, updatePassword, updateProfilePicture, updateUserInfo, updateUserRole } from "../controllers/user.controller";
 import { authorizeRoles, isAuthenticated } from "../middleware/auth";
 const userRouter = express.Router();
 
@@ -27,5 +27,6 @@ userRouter.get("/get-users", isAuthenticated,authorizeRoles("admin"), getAllUser
 
 userRouter.put("/update-user", isAuthenticated, authorizeRoles("admin"), updateUserRole);
 
+userRouter.delete("/delete-user/:id", isAuthenticated, authorizeRoles("admin"), deleteUser);
 
 export default userRouter;
