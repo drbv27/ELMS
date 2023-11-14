91fba9d3b93e947e803bc46551afb1dfa6a6554e
Diego Bonilla <drbv27@gmail.com>
2023-11-14 14:55:06 -0500

    feat: create order

--- server/app.ts
+++ server/app.ts
@@ -6,6 +6,7 @@ import cookieParser from 'cookie-parser';
 import { ErrorMiddleware } from './middleware/error';
 import userRouter from './routes/user.route'
 import courseRouter from './routes/course.route'
+import orderRouter from './routes/order.route';
 
 //body parser
 app.use(express.json({limit: '50mb'}));
@@ -21,9 +22,16 @@ app.use(
 );
 
 //routes
-app.use("/api/v1",userRouter);
-
-app.use("/api/v1",courseRouter);
+// routes
+app.use(
+    "/api/v1",
+    userRouter,
+    orderRouter,
+    courseRouter,
+/*     notificationRouter,
+    analyticsRouter,
+    layoutRouter */
+  );
 
 //testing api
 app.get("/test",(req:Request,res:Response,next:NextFunction) =>{
