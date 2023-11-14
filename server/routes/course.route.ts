2e4076538b4176a8c3ec64f40c386610f7861f13
Diego Bonilla <drbv27@gmail.com>
2023-11-14 11:57:52 -0500

    feat: create question,add reply in question

--- server/routes/course.route.ts
+++ server/routes/course.route.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import {
+    addAnwser,
+    addQuestion,
     editCourse,
     getAllCourses,
     getCourseByUser,
@@ -38,5 +40,9 @@ courseRouter.get("/get-course/:id", getSingleCourse);
 courseRouter.get("/get-courses", getAllCourses);
 
 courseRouter.get("/get-course-content/:id",isAuthenticated, getCourseByUser);
+
+courseRouter.put("/add-question", isAuthenticated, addQuestion);
+
+courseRouter.put("/add-answer", isAuthenticated, addAnwser);
 
 export default courseRouter;
